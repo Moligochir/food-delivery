952037412dafefae8354c6f6363b79f4d5576d3c
25LP5143 25LP5143 <25LP5143@G305-CC0240.local>
2025-11-25 12:34:00 +0800

    sainhii

--- delivery/src/_components/SignUp.js
+++ delivery/src/_components/SignUp.js
@@ -17,143 +17,192 @@ import { useState } from "react";
 
 export const SignUp = () => {
   const [inputUsernameValue, setInputUsernameValue] = useState("");
+  const [inputPasswordValue, setInputPasswordValue] = useState("");
+  const [inputConfirmPasswordValue, setInputConfirmPasswordValue] =
+    useState("");
 
   const [isValid, setIsValid] = useState(true);
+  const [step, setStep] = useState(false);
+  const [isValidPass, setIsValidPass] = useState(true);
+  const [isValidConfirmPass, setIsValidConfirmPass] = useState(true);
 
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$/;
-  const passWordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9\s]).{8,}$/;
-
+  const passWordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9\s]).{8,}$/;
 
   const handleEmailChange = (event) => {
     const newEmail = event.target.value;
     setInputUsernameValue(newEmail);
-    
+
     setIsValid(emailRegex.test(newEmail));
   };
-const handleSubmit = (event) => {
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setInputPasswordValue(newPassword);
+    console.log("1", passWordRegex.test(newPassword), newPassword);
+
+    if (!passWordRegex.test(newPassword)) return setIsValidPass(false);
+
+    setIsValidPass(true);
+  };
+  const handleConfirmPasswordChange = (event) => {
+    const confirmPassword = event.target.value;
+    setInputConfirmPasswordValue(confirmPassword);
+    if (inputPasswordValue !== inputConfirmPasswordValue)
+      console.log("2", inputConfirmPasswordValue, confirmPassword);
+
+    return setIsValidConfirmPass(true);
+  };
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if (isValid) {
-      
-    } 
+    setStep(true);
+  };
+  const handlePreviousButton = () => {
+    setStep(false);
   };
 
-  // const UserLogin = async () => {
-  //   try {
-  //     const response = await fetch(`http://localhost:8000/users/login`, {
-  //       method: "POST",
-  //       headers: {
-  //         accept: "application/json",
-  //         "content-type": "application/json",
-  //         Authorization: "Bearer ",
-  //       },
-  //       body: JSON.stringify({
-  //         email: inputUsernameValue,
-  //         password: inputPasswordValue,
-  //       }),
-  //     });
-
-  //     const jsonData = await response.json();
-  //     console.log(jsonData, "hariu bainuu");
-  //     localStorage.setItem("jsonData", token);
-  //     router.push("/");
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const CreateUser = async () => {
+    try {
+      await fetch(`http://localhost:8000/users`, {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "content-type": "application/json",
+          Authorization: "Bearer ",
+        },
+        body: JSON.stringify({
+          email: inputUsernameValue,
+          password: inputPasswordValue,
+        }),
+      });
+    } catch (err) {}
+  };
 
   return (
     <div className="w-full h-full flex gap-12 pl-50 justify-between items-center">
-      <Card className="w-full max-w-sm ">
-        <CardHeader>
-          <Button variant="outline" size="icon" aria-label="Submit">
-            <ChevronLeftIcon />
-          </Button>
-          <CardTitle>Create your account</CardTitle>
-          <CardDescription>
-            Sign up to explore your favorite dishes.
-          </CardDescription>
-          <CardAction></CardAction>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-4">
-              <div className="grid gap-2">
-                
-                <Input
-                  onChange={handleEmailChange}
-                  id="email"
-                  type="email"
-                  placeholder="Enter your email address"
-                  required
-                />{!isValid && <p className="text-xs" style={{ color: 'red' }}>Invalid email. Use a format like example@email.com</p>}
+      {!step && (
+        <Card className="w-full max-w-sm ">
+          <CardHeader>
+            <Button variant="outline" size="icon" aria-label="Submit">
+              <ChevronLeftIcon />
+            </Button>
+            <CardTitle>Create your account</CardTitle>
+            <CardDescription>
+              Sign up to explore your favorite dishes.
+            </CardDescription>
+            <CardAction></CardAction>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-4">
+                <div className="grid gap-2">
+                  <Input
+                    onChange={handleEmailChange}
+                    id="email"
+                    type="email"
+                    placeholder="Enter your email address"
+                    required
+                  />
+                  {!isValid && (
+                    <p className="text-xs" style={{ color: "red" }}>
+                      Invalid email. Use a format like example@email.com
+                    </p>
+                  )}
+                </div>
               </div>
-              
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Button onClick={handleSubmit} type="submit" className="w-full">
-            Let's Go
-          </Button>
-          <Link href={`/login`}><Button variant="outline" className="w-full text-[#2563EB]">
-            <p className="text-[#71717A]">Already have an account?</p> Log in 
-          </Button>
-          </Link>
-        </CardFooter>
-      </Card>
-      {isValid &&<Card className="w-full max-w-sm ">
-        <CardHeader>
-          <Button variant="outline" size="icon" aria-label="Submit">
-            <ChevronLeftIcon />
-          </Button>
-          <CardTitle>Log in </CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-          <CardAction></CardAction>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-4">
-              <div className="grid gap-2">
-                
-                <Input
-                  onChange={""}
-                  placeholder="Password"
-                  id="password"
-                  type="password"
-                  required
-                />
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button
+              disabled={!isValid}
+              onClick={handleSubmit}
+              type="submit"
+              className="w-full"
+            >
+              Let's Go
+            </Button>
+            <Link href={`/login`}>
+              <Button variant="outline" className="w-full text-[#2563EB]">
+                <p className="text-[#71717A]">Already have an account?</p> Log
+                in
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      )}
+      {step && (
+        <Card className="w-full max-w-sm ">
+          <CardHeader>
+            <Button
+              onClick={handlePreviousButton}
+              variant="outline"
+              size="icon"
+              aria-label="Submit"
+            >
+              <ChevronLeftIcon />
+            </Button>
+            <CardTitle>Create a strong password</CardTitle>
+            <CardDescription>
+              Create a strong password with letters, numbers.
+            </CardDescription>
+            <CardAction></CardAction>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-4">
+                <div className="grid gap-2">
+                  <Input
+                    onChange={handlePasswordChange}
+                    placeholder="Password"
+                    id="password"
+                    type="password"
+                    required
+                  />
+                  {!isValidPass && (
+                    <p className="text-xs" style={{ color: "red" }}>
+                      Weak password. Use numbers and symbols.
+                    </p>
+                  )}
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center"></div>
+                  <Input
+                    onChange={handleConfirmPasswordChange}
+                    placeholder="Password"
+                    id="password"
+                    type="password"
+                    required
+                  />
+                  {!isValidConfirmPass && (
+                    <p className="text-xs" style={{ color: "red" }}>
+                      Those password did’t match, Try again
+                    </p>
+                  )}
+                  {/* <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a> */}
+                </div>
               </div>
-              <div className="grid gap-2">
-                <div className="flex items-center"></div>
-                <Input
-                  onChange={""}
-                  placeholder="Password"
-                  id="password"
-                  type="password"
-                  required
-                />
-                <a
-                  href="#"
-                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Button onClick={""} type="submit" className="w-full">
-            Login
-          </Button >
-          <Button variant="outline" className="w-full text-[#2563EB]">
-            <p className="text-[#71717A]">Don’t have an account?</p> Sign up
-          </Button>
-          
-        </CardFooter>
-      </Card>}
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button
+              disabled={isValidConfirmPass}
+              onClick={""}
+              type="submit"
+              className="w-full"
+            >
+              Let's Go
+            </Button>
+            <Button variant="outline" className="w-full text-[#2563EB]">
+              <p className="text-[#71717A]">Don’t have an account?</p> Sign up
+            </Button>
+          </CardFooter>
+        </Card>
+      )}
 
       <div className="w-full h-full p-5">
         <img src="/Login.png" />
